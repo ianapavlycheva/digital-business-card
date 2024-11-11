795163cdea805942b34511ff02e0a92d886557fc
ianapavlycheva <62348602+ianapavlycheva@users.noreply.github.com>
2024-11-11 19:22:20 +1100

    Add styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,14 +2,15 @@ function About() {
     return (
         <main>
             <img src="./src/images/photo.jpg" className="main--photo" />
-            <p>Frontend Developer</p>
-            <a href="">laurasmith.website</a>
-            <button>Email</button>
             <h1>Laura Smith</h1>
+            <p className="main--position">Frontend Developer</p>
+            <a className="main--link"href="">laurasmith.website</a>
+            <button>Email</button>
+
             <h2>About</h2>
-            <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+            <p className="main--p">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
             <h2>Interests</h2>
-            <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+            <p className="main--p">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
         </main>
     )
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,23 @@
 function Footer() {
     return (
         <footer>
-            <div>
+
                 <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="icon">
-                    <img src="./src/images/Twitter_Icon.png" alt="Twitter Icon" />
+                    <img src="./src/images/Twitter_Icon.png" alt="Twitter Icon" className="img-icon"/>
                 </a>
                 <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="icon">
-                    <img src="./src/images/Facebook_Icon.png" alt="Facebook Icon" />
+                    <img src="./src/images/Facebook_Icon.png" alt="Facebook Icon" className="img-icon" />
                 </a>
                 <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="icon">
-                    <img src="./src/images/Instagram_Icon.png" alt="Instagram Icon" />
+                    <img src="./src/images/Instagram_Icon.png" alt="Instagram Icon" className="img-icon" />
                 </a>
                 <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="icon">
-                    <img src="./src/images/Linkedin_Icon.png" alt="LinkedIn Icon" />
+                    <img src="./src/images/Linkedin_Icon.png" alt="LinkedIn Icon" className="img-icon" />
                 </a>
                 <a href="https://github.com" target="_blank" rel="noopener noreferrer" className="icon">
-                    <img src="./src/images/GitHub_Icon.png" alt="GitHub Icon" />
+                    <img src="./src/images/GitHub_Icon.png" alt="GitHub Icon" className="img-icon" />
                 </a>
-            </div>
+
         </footer>
     )
 }
