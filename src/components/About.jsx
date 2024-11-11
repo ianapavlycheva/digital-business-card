4a2f5ad9eb9a57092509bf2887d6244ab926f170
ianapavlycheva <62348602+ianapavlycheva@users.noreply.github.com>
2024-11-11 16:21:10 +1100

    Add content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,18 @@
 function About() {
-    return <h1>About</h1>
+    return (
+        <main>
+            <img src="./src/images/photo.jpg" className="main--photo" />
+            <p>Frontend Developer</p>
+            <a href="">laurasmith.website</a>
+            <button>Email</button>
+            <h1>Laura Smith</h1>
+            <h2>About</h2>
+            <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+            <h2>Interests</h2>
+            <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+        </main>
+    )
+
 }
 
 export default About
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,25 @@
 function Footer() {
-    return <h1>Footer</h1>
+    return (
+        <footer>
+            <div>
+                <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="icon">
+                    <img src="./src/images/Twitter_Icon.png" alt="Twitter Icon" />
+                </a>
+                <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="icon">
+                    <img src="./src/images/Facebook_Icon.png" alt="Facebook Icon" />
+                </a>
+                <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="icon">
+                    <img src="./src/images/Instagram_Icon.png" alt="Instagram Icon" />
+                </a>
+                <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="icon">
+                    <img src="./src/images/Linkedin_Icon.png" alt="LinkedIn Icon" />
+                </a>
+                <a href="https://github.com" target="_blank" rel="noopener noreferrer" className="icon">
+                    <img src="./src/images/GitHub_Icon.png" alt="GitHub Icon" />
+                </a>
+            </div>
+        </footer>
+    )
 }
 
 export default Footer
